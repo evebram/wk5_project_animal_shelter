ee9c158a62d3e7046b4718e7805a50df9589d6c7
= <evebramley@me.com>
2019-07-19 13:52:45 +0100

    initialized animal class

--- models/animal.rb
+++ models/animal.rb
@@ -1,1 +1,16 @@
 require_relative('../db/sql_runner')
+
+class Animal
+
+  attr_reader :id, :owner_id
+  attr_accessor :animal_name, :breed, :ready_to_adopt, :admission_date, :owner_id
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @animal_name = options['animal_name']
+    @breed = options['breed']
+    @ready_to_adopt = options['ready_to_adopt'].to_
+    @admission_date = options['admission_date'].to_i
+    @owner_id = options['owner_id'].to_i
+  end
+end
