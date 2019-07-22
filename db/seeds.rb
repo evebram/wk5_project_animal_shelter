656c71e4b4958c005373586f326ddba5a3653f82
= <evebramley@me.com>
2019-07-22 15:08:00 +0100

    the update is nearly not broken

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Owner.delete_all()
 Animal.delete_all()
 
 owner1 = Owner.new({
-  "owner_name" => "Trish",
+  "owner_name" => "Trish"
   })
 owner1.save()
 
@@ -34,93 +34,88 @@ owner6 = Owner.new({
   })
 owner6.save()
 
-animal1 = Animal.new({
+animal01 = Animal.new({
   "animal_name" => "Hazel",
   "breed" => "mini-lop",
   "ready_to_adopt" => true,
   "admission_date" => '12.07.2019',
-  "owner_id" => owner1.id,
+  "owner_id" => owner1.id
   })
-animal1.save()
+animal01.save()
 
-animal2 = Animal.new({
+animal02 = Animal.new({
   "animal_name" => "Fiver",
   "breed" => "netherlands dwarf",
   "ready_to_adopt" => true,
   "admission_date" => '05.06.2019',
-  "owner_id" => owner1.id,
+  "owner_id" => owner1.id
   })
-animal2.save()
+animal02.save()
 
-animal3 = Animal.new({
+animal03 = Animal.new({
   "animal_name" => "Bigwig",
   "breed" => "flemish giant",
   "ready_to_adopt" => false,
-  "admission_date" => '06.05.2019',
-  "owner_id" => "",
+  "admission_date" => '06.05.2019'
   })
-animal3.save()
+animal03.save()
 
-animal4 = Animal.new({
+animal04 = Animal.new({
   "animal_name" => "Silver",
   "breed" => "rex",
   "ready_to_adopt" => true,
   "admission_date" => '28.03.2019',
-  "owner_id" => owner3.id,
+  "owner_id" => owner3.id
   })
-animal4.save()
+animal04.save()
 
-animal5 = Animal.new({
+animal05 = Animal.new({
   "animal_name" => "Blackberry",
   "breed" => "netherlands dwarf",
   "ready_to_adopt" => true,
   "admission_date" => '28.03.2019',
-  "owner_id" => owner5.id,
+  "owner_id" => owner5.id
   })
-animal5.save()
+animal05.save()
 
-animal6 = Animal.new({
+animal06 = Animal.new({
   "animal_name" => "Dandelion",
   "breed" => "lionhead",
   "ready_to_adopt" => true,
   "admission_date" => '15.04.2019',
-  "owner_id" => owner2.id,
+  "owner_id" => owner2.id
   })
-animal6.save()
+animal06.save()
 
-animal7 = Animal.new({
+animal07 = Animal.new({
   "animal_name" => "Pipkin",
   "breed" => "mini-lop",
   "ready_to_adopt" => true,
-  "admission_date" => '21.05.2019',
-  "owner_id" => "",
+  "admission_date" => '21.05.2019'
   })
-animal7.save()
+animal07.save()
 
-animal8 = Animal.new({
+animal08 = Animal.new({
   "animal_name" => "Buckthorn",
   "breed" => "netherlands dwarf",
   "ready_to_adopt" => false,
-  "admission_date" => '01.06.2019',
-  "owner_id" => "",
+  "admission_date" => '01.06.2019'
   })
-animal8.save()
+animal08.save()
 
-animal9 = Animal.new({
+animal09 = Animal.new({
   "animal_name" => "Strawberry",
   "breed" => "angora",
   "ready_to_adopt" => false,
-  "admission_date" => '25.05.2019',
-  "owner_id" => "",
+  "admission_date" => '25.05.2019'
   })
-animal9.save()
+animal09.save()
 
 animal10 = Animal.new({
   "animal_name" => "Hawkbit",
   "breed" => "Californian",
   "ready_to_adopt" => true,
-  "admission_date" => '15.06.2019',
-  "owner_id" => "",
+  "admission_date" => '15.06.2019'
   })
 animal10.save()
 
@@ -128,7 +123,6 @@ animal11 = Animal.new({
   "animal_name" => "Holly",
   "breed" => "Dutch",
   "ready_to_adopt" => false,
-  "admission_date" => '10.07.2019',
-  "owner_id" => "",
+  "admission_date" => '10.07.2019'
   })
 animal11.save()
