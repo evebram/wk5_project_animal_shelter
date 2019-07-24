681ef05af11428d59812fcb2d2d9a6f95f64d627
= <evebramley@me.com>
2019-07-24 09:36:22 +0100

    updated shelter.sql

--- db/shelter.sql
+++ db/shelter.sql
@@ -11,6 +11,7 @@ CREATE TABLE animals(
   id SERIAL4 PRIMARY KEY,
   animal_name VARCHAR(255),
   breed VARCHAR(255),
+  age INT4,
   ready_to_adopt BOOLEAN,
   admission_date VARCHAR(255),
   owner_id INT4 REFERENCES owners(id) ON DELETE CASCADE
